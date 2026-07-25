63537320ef29819b2da81d8b9816107c6a37dc4e
saravanan14-max <saravchandra7@gmail.com>
2026-07-25 19:59:44 +0530

    Add files via upload

---
+++ day14/Interface.java
@@ -0,0 +1,14 @@
+package day14;
+interface A{
+    void a();
+}
+interface C extends A{
+    void display();
+}
+class B extends C{
+    public class a(){
+        System.out.println("h");
+    }
+}
+public class Interface {
+}
